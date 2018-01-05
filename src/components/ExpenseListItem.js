a6b9accbb35793355ab1acd69e299f28bb40e7bf
Jim Mortenson <jmortenson@dealersocket.com>
2018-01-05 15:24:25 -0600

    Formatting dates

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <p>
@@ -7,7 +8,7 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <Link to={`/edit/${id}`}>{description}</Link>
     </span>
     <span className="ExpenseItem__amount">{amount}</span>
-    <span className="ExpenseItem__createdAt">{createdAt}</span>
+    <span className="ExpenseItem__createdAt">{moment(createdAt).format('MMM DD YYYY')}</span>
   </p>
 )
 
