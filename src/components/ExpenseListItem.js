8abcecf241ab154e24010567ed029d412392a3dd
morty <user@JHM-MacBookPro.local>
2018-01-07 18:06:01 -0600

    Added numeral.js lib for currency formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <p>
     <span className="ExpenseItem__description">
     <Link to={`/edit/${id}`}>{description}</Link>
     </span>
-    <span className="ExpenseItem__amount">{amount}</span>
+    <span className="ExpenseItem__amount">{numeral(amount / 100).format('$0,0.00')}</span>
     <span className="ExpenseItem__createdAt">{moment(createdAt).format('MMM DD YYYY')}</span>
   </p>
 )
